a6fe7cda8ef8b041f6bc172af7f198221109b539
Raihan Meisan P <saptoharsongko835@gmail.com>
2022-04-24 09:30:25 +0700

    NFT Challenge Youtube

---
+++ pages/index.tsx
@@ -0,0 +1,17 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+
+const Home: NextPage = () => {
+  return (
+    <div className="">
+      <Head>
+        <title>NFT Drop</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <h1>NFT Drop</h1>
+    </div>
+  )
+}
+
+export default Home
